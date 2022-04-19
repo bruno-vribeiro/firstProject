d014122c75014f666776c28c4f4d82f96af29d6a
Bruno Ribeiro <vilhenab8gmail.com>
2022-04-19 08:15:01 -0300

    Integrado o extrato com API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'firstProject';
 
-  transferencias:any[]=[];
+  constructor(private service: TransferenciaService){}
 
   transferir($event){
-    console.log($event);
-    const transferencia = {...$event, data: new Date(new Date())};
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event)
+
 
   }
 }
